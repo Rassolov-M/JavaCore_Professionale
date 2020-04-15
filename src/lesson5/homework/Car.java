87e820c33d0d5114b99574fae0af5c7bc619a97b
Mikhail Rassolov <aironpreest666@gmail.com>
2020-04-15 14:08:13 +0300

    hw5 многопоточность переделанный проект гонки

--- src/lesson5/homework/Car.java
+++ src/lesson5/homework/Car.java
@@ -1,33 +1,56 @@
 package lesson5.homework;
 
+import java.util.concurrent.BrokenBarrierException;
+
 public class Car implements Runnable {
+
     private static int CARS_COUNT;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
     private Race race;
     private int speed;
     private String name;
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
-    public Car(Race race, int speed) {
-        this.race = race;
+
+    public Car(int speed) {
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void addToRace(Race race) {
+        this.race = race;
+    }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
+
             System.out.println(this.name + " готов");
-        } catch (Exception e) {
+            race.getStartSignal().await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            race.setWinner(this);
+            race.getFinishSignal().await();
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        } finally {
+            Thread.currentThread().interrupt();
         }
     }
 }
